0a665d8f4af88d06e27a4ebedaccb6f2f1da564c
Paul Marsicovetere <paul.marsicovetere@formidable.com>
2022-11-29 12:05:26 -0500

    multiple region tests

--- test/localstack.tf
+++ test/localstack.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   access_key                  = "test"
   secret_key                  = "test"
-  region                      = "us-east-1"
   s3_use_path_style           = false
   skip_credentials_validation = true
   skip_metadata_api_check     = true
--- test/terraform_aws_platform_test.go
+++ test/terraform_aws_platform_test.go
@@ -1,15 +1,43 @@
 package test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTerraformAWSPlatform(t *testing.T) {
+func TestTerraformAWSPlatformUsWest2(t *testing.T) {
 	// Construct the terraform options with default retryable errors to handle the most common
 	// retryable errors in terraform testing.
+	os.Setenv("AWS_REGION", "us-west-2")
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "../",
+	})
+
+	// Clean up resources with "terraform destroy" at the end of the test.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables and check they have the expected values.
+	accountId := terraform.Output(t, terraformOptions, "account_id")
+	assert.Equal(t, "000000000000", accountId)
+	partition := terraform.Output(t, terraformOptions, "partition")
+	assert.Equal(t, "aws", partition)
+	region := terraform.Output(t, terraformOptions, "region")
+	assert.Equal(t, "us-west-2", region)
+	regionShort := terraform.Output(t, terraformOptions, "region_short")
+	assert.Equal(t, "usw2", regionShort)
+}
+
+func TestTerraformAWSPlatformUsEast1(t *testing.T) {
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+	os.Setenv("AWS_REGION", "us-east-1")
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// Set the path to the Terraform code that will be tested.
 		TerraformDir: "../",
